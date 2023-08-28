1b358ed55e3771cd2b11c5f13e34216f01269c2c
Damian Bossak <damian.bossak@innovid.com>
2023-08-28 17:27:57 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Code Clan - Event
+
+## Installation
+
+- `git clone`
+- cd into project
+- `npm i` (please use node 16 or higher)
+- `npm run dev`
+- By default, Vite will run on `http://localhost:5173/`
+
+## Guidelines
+
+- for each ticket you need to create a new branch
+- branch naming convention: `task/TIC-X/some-description`
+- commit naming convention: `TIC-X Commit message`
+- do not merge your own branches into `main`
+- Only Merge Reviewer can merge into `main`
---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+const fooCampaigns = [
+  {
+    id: 16346145,
+    title: 'Campaign 1',
+    startdate: '01/01/2023',
+    enddate: '31/02/2023',
+    totalImpressions: 1113,
+    totalResponse: 11008,
+  },
+  {
+    id: 16346145,
+    title: 'Campaign 2',
+    startdate: '01/04/2023',
+    enddate: '27/08/2023',
+    totalImpressions: 1234551113,
+    totalResponse: 55611008,
+  },
+  {
+    id: 16346145,
+    title: 'Campaign 3',
+    startdate: '09/09/2021',
+    enddate: '31/10/2021',
+    totalImpressions: 12133,
+    totalResponse: 13408,
+  },
+];
+
+function App() {
+  const campaignList = fooCampaigns.map((campaign) => (
+    <li key={campaign.id}>
+      {campaign.title} | start date: {campaign.startdate} | end date:{' '}
+      {campaign.enddate} | Total Impressions {campaign.totalImpressions} | Total
+      Responses {campaign.totalResponse}
+    </li>
+  ));
+
+  return (
+    <>
+      <h1>Code Clan - event</h1>
+      <ul>{campaignList}</ul>
+    </>
+  );
+}
+
+export default App;
