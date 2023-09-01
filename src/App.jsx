3b5ca234cfdd8b3541a04e11614432a49d326fd7
Andrew Blair <a.a.g.blair@gmail.com>
2023-09-01 14:32:42 +0100

    numbers formatted with commas

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,58 @@
+import { useState, useEffect } from "react"
+
 const fooCampaigns = [
   {
     id: 16346145,
-    title: "Campaign 1",
-    startdate: "01/01/2023",
-    enddate: "31/02/2023",
+    title: 'Campaign 1',
+    startdate: '01/01/2023',
+    enddate: '31/02/2023',
     totalImpressions: 1113,
     totalResponse: 11008,
   },
   {
-    id: 16346146,
-    title: "Campaign 2",
-    startdate: "01/04/2023",
-    enddate: "27/08/2023",
+    id: 16346145,
+    title: 'Campaign 2',
+    startdate: '01/04/2023',
+    enddate: '27/08/2023',
     totalImpressions: 1234551113,
     totalResponse: 55611008,
   },
   {
-    id: 16346147,
-    title: "Campaign 3",
-    startdate: "09/09/2021",
-    enddate: "31/10/2021",
+    id: 16346145,
+    title: 'Campaign 3',
+    startdate: '09/09/2021',
+    enddate: '31/10/2021',
     totalImpressions: 12133,
     totalResponse: 13408,
   },
 ];
 
 function App() {
+
+//   const express = require('express');
+// const app = express();
+
+// ADDED
+// const cors = require('cors');
+// app.use(cors());
+
+//   const [fooCampaigns, setFooCampaigns] = useState([])
+  
+//   useEffect(() => {
+//   fetch("https://cclan.s3.eu-west-1.amazonaws.com/campaigns.json")
+//     .then((res) => res.json())
+//     .then((data) => setFooCampaigns(data))
+//   });
+
+  function numberFormatChange(number){
+    return number.toLocaleString("en-US");
+  }
+
   const campaignList = fooCampaigns.map((campaign) => (
     <li key={campaign.id}>
-      {campaign.title} | start date: {campaign.startdate} | end date:{" "}
-      {campaign.enddate} | Total Impressions {campaign.totalImpressions} | Total
-      Responses {campaign.totalResponse}
+      {campaign.title} | start date: {campaign.startdate} | end date:{' '}
+      {campaign.enddate} | Total Impressions {numberFormatChange(campaign.totalImpressions)} | Total
+      Responses {numberFormatChange(campaign.totalResponse)}
     </li>
   ));
 
