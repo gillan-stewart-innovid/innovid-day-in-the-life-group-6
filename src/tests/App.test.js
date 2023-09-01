0719299141fbe7903da10d41e1bb064a1d60d440
Andrew Blair <a.a.g.blair@gmail.com>
2023-09-01 16:10:29 +0100

    beginning of test file created, no actual tests though

---
+++ src/tests/App.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import App from '../App';
+import {render, fireEvent, waitFor} from '@testing-library/react';
+
+describe('App', () => {
+    let container;
+    beforeEach(() => {
+        container = render(<App />)
+    })
+
+})
+
