64c86b007fb820bdd71cad8ed9560d793f23a55b
John <ashton.basile@gmail.com>
2023-09-01 15:50:48 +0100

    I turned some functions into components

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,71 @@
-import { useState, useEffect } from "react"
+import { useState, useEffect } from 'react';
 
-function dateFormatChange(date){
-  const output = date.split("/")
-  const days = output[0]
-  const months = output[1]
-  const years = output[2]
-  return months + '/' + days + '/' + years
+function dateFormatChange(date) {
+  const output = date.split('/');
+  const days = output[0];
+  const months = output[1];
+  const years = output[2];
+  return months + '/' + days + '/' + years;
 }
 
 function App() {
-
-  const [fooCampaigns, setFooCampaigns] = useState([])
+  const [fooCampaigns, setFooCampaigns] = useState([]);
 
   useEffect(() => {
-  fetch("./server/campaigns.json")
-    .then((res) => res.json())
-    .then((data) => setFooCampaigns(data))
-  fetch("./server/results.json")
-    .then((res) => res.json())
-    .then((data) => console.log(data))
+    fetch('./server/campaigns.json')
+      .then((res) => res.json())
+      .then((data) => setFooCampaigns(data));
+    fetch('./server/results.json')
+      .then((res) => res.json())
+      .then((data) => console.log(data));
   });
-
 
   const campaignList = fooCampaigns.map((campaign) => (
     <li key={campaign.id}>
-
-      {campaign.title} | start date: {dateFormatChange(campaign.startdate) ? campaign.startdate : 'Invalid Date'} | end date:{' '}
-      {dateFormatChange(campaign.enddate) ? campaign.enddate : 'Invalid Date'} | Total Impressions {campaign.totalImpressions} | Total
-
-      Responses {campaign.totalResponse}
+      {campaign.title} | start date:{' '}
+      {dateFormatChange(campaign.startdate)
+        ? campaign.startdate
+        : 'Invalid Date'}{' '}
+      | end date:{' '}
+      {dateFormatChange(campaign.enddate) ? campaign.enddate : 'Invalid Date'} |
+      Total Impressions {campaign.totalImpressions} | Total Responses{' '}
+      {campaign.totalResponse}
     </li>
   ));
 
+  const TotalImpressionsAddUp = () => {
+    return (
+      <p>
+        Total Impressions for all campaigns:{' '}
+        {fooCampaigns.reduce(
+          (accImp, campaign) =>
+            accImp +
+            (campaign.totalImpressions ? campaign.totalImpressions : 0),
+          0,
+        )}
+      </p>
+    );
+  };
 
-  const totalImpressionsAddUp = <p>Total Impressions for all campaigns: {fooCampaigns.reduce((accImp, campaign) => ( accImp + (campaign.totalImpressions ? campaign.totalImpressions : 0)),0)}</p>
-  const totalResponsesAddUp = <p>Total Responses for all campaigns: {fooCampaigns.reduce((accImp, campaign) => ( accImp + (campaign.totalResponse ? campaign.totalResponse : 0)),0)}</p>
-  
+  const TotalResponsesAddUp = () => {
+    return (
+      <p>
+        Total Responses for all campaigns:{' '}
+        {fooCampaigns.reduce(
+          (accImp, campaign) =>
+            accImp + (campaign.totalResponse ? campaign.totalResponse : 0),
+          0,
+        )}
+      </p>
+    );
+  };
 
   return (
     <>
       <h1>Code Clan - event</h1>
       <ul>{campaignList}</ul>
-      <p>{totalImpressionsAddUp}</p>
-      <p>{totalResponsesAddUp}</p>
+      <TotalImpressionsAddUp />
+      <TotalResponsesAddUp />
     </>
   );
 }
